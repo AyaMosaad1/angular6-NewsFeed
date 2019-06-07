1563722aa58b4455a68b4bb504e9e175fa1bef83
AyaMosaad1 <Ayamosad37@gmail.com>
2019-06-07 09:03:42 +0200

    some features

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -22,59 +22,16 @@ import {
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements
-  OnInit,
-  OnChanges,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked,
-  OnDestroy {
+export class ServerElementComponent implements OnInit
+ {
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading') header: ElementRef;
   @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() {
-    console.log('constructor called!');
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called!');
-    console.log(changes);
   }
 
   ngOnInit() {
-    console.log('ngOnInit called!');
-    console.log('Text Content: ' + this.header.nativeElement.textContent);
-    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
-
-  ngDoCheck() {
-    console.log('ngDoCheck called!');
-  }
-
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit called!');
-    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
-  }
-
-  ngAfterContentChecked() {
-    console.log('ngAfterContentChecked called!');
-  }
-
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit called!');
-    console.log('Text Content: ' + this.header.nativeElement.textContent);
-  }
-
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called!');
-  }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy called!');
-  }
-
 }
